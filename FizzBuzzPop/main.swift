11f7d5a66843dae542674fd0bd84aa7a2ee5bbdc
daukesnurmukhan <daukesnurmukhan@gmail.com>
2022-05-01 13:11:27 +0600

    Initial Commit

--- FizzBuzzPop/main.swift
+++ FizzBuzzPop/main.swift
@@ -1,11 +1,28 @@
-//
-//  main.swift
-//  FizzBuzzPop
-//
-//  Created by Нұрмұхан Дәукес on 30.04.2022.
-//
+import Darwin
 
-import Foundation
+print("please, write the exact meaning: \n")
 
-print("Hello, World!")
+guard let meaning = readLine(),
+      
+      let Meaning = Int(meaning) else {
+    print("Invalid Input")
+        exit(0)
+}
+
+if Meaning % 3 == 0 && Meaning % 5 == 0 && Meaning % 7 == 0 {
+    print("fizz buzz pop")
+} else if Meaning % 3 == 0 && Meaning % 5 == 0 {
+    print("fizz buzz")
+} else if Meaning % 3 == 0 && Meaning % 7 == 0 {
+    print("fizz pop")
+} else if Meaning % 5 == 0 && Meaning % 7 == 0 {
+    print("buzz pop")
+} else if Meaning % 3 == 0 {
+        print("fizz")
+} else if Meaning % 5 == 0 {
+        print("buzz")
+} else if Meaning % 7 == 0 {
+        print("pop")
+}
+
 
